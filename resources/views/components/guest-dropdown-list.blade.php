011cb2c7c5cea03bc03d637981f0a3d8e3189c9b
Marian Dumitru <d.mariandev@gmail.com>
2024-09-12 22:21:37 +0300

    auth, admin posts, edit, create

--- resources/views/components/guest-dropdown-list.blade.php
+++ resources/views/components/guest-dropdown-list.blade.php
@@ -1,6 +1,8 @@
 <ul class="py-2" aria-labelledby="user-menu-button">
     <li>
         <a href="{{ route('login') }}"
-           class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">{{ __('auth.login') }}</a>
+           class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+            Login
+        </a>
     </li>
 </ul>
