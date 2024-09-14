5234653dc9997b2ecd9310243c316c422d98ca28
Marian Dumitru <d.mariandev@gmail.com>
2024-09-14 14:43:16 +0300

    global variables

--- resources/views/components/user-menu.blade.php
+++ resources/views/components/user-menu.blade.php
@@ -4,10 +4,27 @@
             id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
             data-dropdown-placement="bottom">
         <span class="sr-only">Open user menu</span>
-        <div
-            class="relative inline-flex items-center justify-center w-16 h-16 overflow-hidden bg-red-300 rounded-full dark:bg-gray-600">
-            <span class="text-xl font-bold text-gray-600 dark:text-gray-300">MD</span>
-        </div>
+        @if($isUserAuthenticated)
+            @if($user->getMedia('profile_pictures')->count() > 0)
+                <div class="text-center">
+                    <img
+                        src="{{ $user->getFirstMediaUrl('profile_pictures') }}"
+                        class="mx-auto mb-4 w-16 h-16 rounded-full"
+                        alt="Avatar" />
+                    <h5 class="mb-2 text-sm font-medium leading-tight">{{ $user->getFullName() }}</h5>
+                </div>
+            @else
+                <div
+                    class="relative inline-flex items-center justify-center w-16 h-16 overflow-hidden bg-red-300 rounded-full dark:bg-gray-600">
+                    <span class="text-xl font-bold text-gray-600 dark:text-gray-300">{{ $initials }}</span>
+                </div>
+            @endif
+        @else
+            <img
+                src="{{ url('/avatar.png') }}"
+                class="mx-auto mb-4 w-16 h-16 rounded-full"
+                alt="Avatar" />
+        @endif
     </button>
     @include('components.dropdown-menu')
     @include('components.mobile-dropdown-menu')
