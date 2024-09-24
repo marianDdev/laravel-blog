8ec242f8b331ee2c224d63a7d6d14ac46a5de301
Marian Dumitru <d.mariandev@gmail.com>
2024-09-24 18:10:59 +0300

    fix create post link

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -21,7 +21,7 @@
                     </address>
                     <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">{{ $post->title }}</h1>
                 </header>
-                <p class="lead">{{ $post->summary }}</p>
+                <p class="lead">{!! $post->summary !!}</p>
                 <p>{!! $post->first_paragraph !!}</p>
                 <p>{!! $post->second_paragraph!!}</p>
                 <p>{!! $post->third_paragraph !!}</p>
